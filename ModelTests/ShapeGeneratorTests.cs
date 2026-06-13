65c80310aa3d1c822048b056839f052d709af488
Maksimka <mdr10@tpu.ru>
2026-06-13 14:57:29 +0700

    LR6 fix

--- ModelTests/ShapeGeneratorTests.cs
+++ ModelTests/ShapeGeneratorTests.cs
@@ -3,51 +3,58 @@ using NUnit.Framework;
 
 namespace ModelTests
 {
+    /// <summary>
+    /// Класс для тестирования класса ShapeGenerator.
+    /// </summary>
     public class ShapeGeneratorTests
     {
-
-        [Test(Description = "GetRandomCircle возвращает Circle с положительным радиусом")]
+        [Test(Description =
+            "GetRandomCircle возвращает Circle с положительным радиусом")]
         public void GetRandomCircleReturnsCircleWithPositiveRadius()
         {
             var circle = ShapeGenerator.GetRandomCircle();
-
             Assert.IsNotNull(circle);
             Assert.IsInstanceOf<Circle>(circle);
             Assert.Greater(circle.Radius, 0);
         }
 
-        [Test(Description = "GetRandomRectangle возвращает Rectangle с положительными сторонами")]
+        [Test(Description =
+            "GetRandomRectangle возвращает Rectangle " +
+            "с положительными сторонами")]
         public void GetRandomRectangleReturnsRectangleWithPositiveSides()
         {
             var rect = ShapeGenerator.GetRandomRectangle();
-
             Assert.IsNotNull(rect);
             Assert.IsInstanceOf<Rectangle>(rect);
             Assert.Greater(rect.Width, 0);
             Assert.Greater(rect.Height, 0);
         }
 
-        [Test(Description = "GetRandomTriangle возвращает Triangle с валидными сторонами")]
+        [Test(Description =
+            "GetRandomTriangle возвращает Triangle с валидными сторонами")]
         public void GetRandomTriangleReturnsTriangleWithValidSides()
         {
             var triangle = ShapeGenerator.GetRandomTriangle();
-
             Assert.IsNotNull(triangle);
             Assert.IsInstanceOf<Triangle>(triangle);
             Assert.Greater(triangle.SideA, 0);
             Assert.Greater(triangle.SideB, 0);
             Assert.Greater(triangle.SideC, 0);
-            Assert.IsTrue(triangle.SideA + triangle.SideB > triangle.SideC);
-            Assert.IsTrue(triangle.SideA + triangle.SideC > triangle.SideB);
-            Assert.IsTrue(triangle.SideB + triangle.SideC > triangle.SideA);
+            Assert.IsTrue(
+                triangle.SideA + triangle.SideB > triangle.SideC);
+            Assert.IsTrue(
+                triangle.SideA + triangle.SideC > triangle.SideB);
+            Assert.IsTrue(
+                triangle.SideB + triangle.SideC > triangle.SideA);
         }
 
-        [Test(Description = "GetRandomShape возвращает ShapeBase (одну из трёх фигур)")]
+        [Test(Description =
+            "GetRandomShape возвращает ShapeBase (одну из трёх фигур)")]
         public void GetRandomShapeReturnsShapeBase()
         {
-            bool hasCircle = false;
+            bool hasCircle    = false;
             bool hasRectangle = false;
-            bool hasTriangle = false;
+            bool hasTriangle  = false;
 
             for (int i = 0; i < 300; i++)
             {
@@ -63,16 +70,19 @@ namespace ModelTests
                     break;
             }
 
-            Assert.IsTrue(hasCircle,    "GetRandomShape никогда не вернул Circle");
-            Assert.IsTrue(hasRectangle, "GetRandomShape никогда не вернул Rectangle");
-            Assert.IsTrue(hasTriangle,  "GetRandomShape никогда не вернул Triangle");
+            Assert.IsTrue(hasCircle,
+                "GetRandomShape никогда не вернул Circle");
+            Assert.IsTrue(hasRectangle,
+                "GetRandomShape никогда не вернул Rectangle");
+            Assert.IsTrue(hasTriangle,
+                "GetRandomShape никогда не вернул Triangle");
         }
 
-        [Test(Description = "GetRandomShape возвращает фигуру с положительной площадью")]
+        [Test(Description =
+            "GetRandomShape возвращает фигуру с положительной площадью")]
         public void GetRandomShapeReturnsShapeWithPositiveArea()
         {
             var shape = ShapeGenerator.GetRandomShape();
-
             Assert.Greater(shape.CalculateArea(), 0);
         }
     }
--- ModelTests/TriangleTests.cs
+++ ModelTests/TriangleTests.cs
@@ -3,33 +3,38 @@ using NUnit.Framework;
 
 namespace ModelTests
 {
+    /// <summary>
+    /// Класс для тестирования класса Triangle.
+    /// </summary>
     public class TriangleTests
     {
-
-        [TestCase(3.0, 4.0, 5.0,  TestName = "Создание Triangle 3-4-5 (прямоугольный)")]
-        [TestCase(5.0, 5.0, 5.0,  TestName = "Создание Triangle 5-5-5 (равносторонний)")]
-        [TestCase(6.0, 8.0, 10.0, TestName = "Создание Triangle 6-8-10")]
+        [TestCase(3.0, 4.0, 5.0,
+            TestName = "Создание Triangle 3-4-5 (прямоугольный)")]
+        [TestCase(5.0, 5.0, 5.0,
+            TestName = "Создание Triangle 5-5-5 (равносторонний)")]
+        [TestCase(6.0, 8.0, 10.0,
+            TestName = "Создание Triangle 6-8-10")]
         public void ConstructorWithSidesSetsProperties(
             double sideA, double sideB, double sideC)
         {
             var triangle = new Triangle(sideA, sideB, sideC);
-
             Assert.AreEqual(sideA, triangle.SideA, TestTools.Tolerance);
             Assert.AreEqual(sideB, triangle.SideB, TestTools.Tolerance);
             Assert.AreEqual(sideC, triangle.SideC, TestTools.Tolerance);
         }
 
-        [Test(Description = "Конструктор по умолчанию создаёт Triangle без исключений")]
+        [Test(Description =
+            "Конструктор по умолчанию создаёт Triangle без исключений")]
         public void DefaultConstructorCreatesTriangleWithoutException()
         {
             Assert.DoesNotThrow(() => new Triangle());
         }
 
-        [Test(Description = "Validate на корректном Triangle не выбрасывает исключений")]
+        [Test(Description =
+            "Validate на корректном Triangle не выбрасывает исключений")]
         public void ValidateOnValidTriangleDoesNotThrow()
         {
             var triangle = new Triangle(3.0, 4.0, 5.0);
-
             Assert.DoesNotThrow(() => triangle.Validate());
         }
 
@@ -57,47 +62,54 @@ namespace ModelTests
             Assert.AreEqual(9.0, triangle.SideC, TestTools.Tolerance);
         }
 
-        [TestCase(3.0, 4.0, 5.0,  6.0,          TestName = "CalculateArea для 3-4-5 = 6.0")]
-        [TestCase(5.0, 5.0, 5.0,  10.825317547,  TestName = "CalculateArea для 5-5-5")]
-        [TestCase(6.0, 8.0, 10.0, 24.0,          TestName = "CalculateArea для 6-8-10 = 24.0")]
+        [TestCase(3.0, 4.0, 5.0, 6.0,
+            TestName = "CalculateArea для 3-4-5 = 6.0")]
+        [TestCase(5.0, 5.0, 5.0, 10.825317547,
+            TestName = "CalculateArea для равностороннего 5-5-5")]
+        [TestCase(6.0, 8.0, 10.0, 24.0,
+            TestName = "CalculateArea для 6-8-10 = 24.0")]
         public void CalculateAreaReturnsCorrectValue(
-            double sideA, double sideB, double sideC, double expectedArea)
+            double sideA, double sideB,
+            double sideC, double expectedArea)
         {
             var triangle = new Triangle(sideA, sideB, sideC);
-
             double area = triangle.CalculateArea();
-
             Assert.AreEqual(expectedArea, area, TestTools.Tolerance);
         }
 
-        [TestCase(3.0, 4.0, 5.0, TestName = "GetInfo содержит стороны и площадь для 3-4-5")]
-        [TestCase(5.0, 5.0, 5.0, TestName = "GetInfo содержит стороны и площадь для 5-5-5")]
-        public void GetInfoContainsSidesAndArea(double sideA, double sideB, double sideC)
+        [TestCase(3.0, 4.0, 5.0,
+            TestName = "GetInfo содержит стороны и площадь для 3-4-5")]
+        [TestCase(5.0, 5.0, 5.0,
+            TestName = "GetInfo содержит стороны и площадь для 5-5-5")]
+        public void GetInfoContainsSidesAndArea(
+            double sideA, double sideB, double sideC)
         {
             var triangle = new Triangle(sideA, sideB, sideC);
-
             string info = triangle.GetInfo();
-
             Assert.IsTrue(info.Contains(sideA.ToString("F2")));
             Assert.IsTrue(info.Contains(sideB.ToString("F2")));
             Assert.IsTrue(info.Contains(sideC.ToString("F2")));
-            Assert.IsTrue(info.Contains(triangle.CalculateArea().ToString("F2")));
+            Assert.IsTrue(
+                info.Contains(triangle.CalculateArea().ToString("F2")));
         }
 
-        [Test(Description = "GetShapeType возвращает «Треугольник»")]
+        [Test(Description = "GetShapeType возвращает \"Треугольник\"")]
         public void GetShapeTypeReturnsTreeugolnik()
         {
             var triangle = new Triangle(3.0, 4.0, 5.0);
-
             Assert.AreEqual("Треугольник", triangle.GetShapeType());
         }
 
-
-        [TestCase(-1.0,                    4.0, 5.0, TestName = "Создание Triangle со стороной A = -1.0")]
-        [TestCase(0.0,                     4.0, 5.0, TestName = "Создание Triangle со стороной A = 0.0")]
-        [TestCase(double.NaN,              4.0, 5.0, TestName = "Создание Triangle со стороной A = NaN")]
-        [TestCase(double.PositiveInfinity, 4.0, 5.0, TestName = "Создание Triangle со стороной A = +Infinity")]
-        [TestCase(double.NegativeInfinity, 4.0, 5.0, TestName = "Создание Triangle со стороной A = -Infinity")]
+        [TestCase(-1.0, 4.0, 5.0,
+            TestName = "Создание Triangle со стороной A = -1.0")]
+        [TestCase(0.0, 4.0, 5.0,
+            TestName = "Создание Triangle со стороной A = 0.0")]
+        [TestCase(double.NaN, 4.0, 5.0,
+            TestName = "Создание Triangle со стороной A = NaN")]
+        [TestCase(double.PositiveInfinity, 4.0, 5.0,
+            TestName = "Создание Triangle со стороной A = +Infinity")]
+        [TestCase(double.NegativeInfinity, 4.0, 5.0,
+            TestName = "Создание Triangle со стороной A = -Infinity")]
         public void ConstructorWithInvalidSideAThrowsArgumentException(
             double invalidSideA, double sideB, double sideC)
         {
@@ -105,12 +117,16 @@ namespace ModelTests
                 () => new Triangle(invalidSideA, sideB, sideC));
         }
 
-
-        [TestCase(3.0, -1.0,                    5.0, TestName = "Создание Triangle со стороной B = -1.0")]
-        [TestCase(3.0, 0.0,                     5.0, TestName = "Создание Triangle со стороной B = 0.0")]
-        [TestCase(3.0, double.NaN,              5.0, TestName = "Создание Triangle со стороной B = NaN")]
-        [TestCase(3.0, double.PositiveInfinity, 5.0, TestName = "Создание Triangle со стороной B = +Infinity")]
-        [TestCase(3.0, double.NegativeInfinity, 5.0, TestName = "Создание Triangle со стороной B = -Infinity")]
+        [TestCase(3.0, -1.0, 5.0,
+            TestName = "Создание Triangle со стороной B = -1.0")]
+        [TestCase(3.0, 0.0, 5.0,
+            TestName = "Создание Triangle со стороной B = 0.0")]
+        [TestCase(3.0, double.NaN, 5.0,
+            TestName = "Создание Triangle со стороной B = NaN")]
+        [TestCase(3.0, double.PositiveInfinity, 5.0,
+            TestName = "Создание Triangle со стороной B = +Infinity")]
+        [TestCase(3.0, double.NegativeInfinity, 5.0,
+            TestName = "Создание Triangle со стороной B = -Infinity")]
         public void ConstructorWithInvalidSideBThrowsArgumentException(
             double sideA, double invalidSideB, double sideC)
         {
@@ -118,12 +134,16 @@ namespace ModelTests
                 () => new Triangle(sideA, invalidSideB, sideC));
         }
 
-
-        [TestCase(3.0, 4.0, -1.0,                    TestName = "Создание Triangle со стороной C = -1.0")]
-        [TestCase(3.0, 4.0, 0.0,                     TestName = "Создание Triangle со стороной C = 0.0")]
-        [TestCase(3.0, 4.0, double.NaN,              TestName = "Создание Triangle со стороной C = NaN")]
-        [TestCase(3.0, 4.0, double.PositiveInfinity, TestName = "Создание Triangle со стороной C = +Infinity")]
-        [TestCase(3.0, 4.0, double.NegativeInfinity, TestName = "Создание Triangle со стороной C = -Infinity")]
+        [TestCase(3.0, 4.0, -1.0,
+            TestName = "Создание Triangle со стороной C = -1.0")]
+        [TestCase(3.0, 4.0, 0.0,
+            TestName = "Создание Triangle со стороной C = 0.0")]
+        [TestCase(3.0, 4.0, double.NaN,
+            TestName = "Создание Triangle со стороной C = NaN")]
+        [TestCase(3.0, 4.0, double.PositiveInfinity,
+            TestName = "Создание Triangle со стороной C = +Infinity")]
+        [TestCase(3.0, 4.0, double.NegativeInfinity,
+            TestName = "Создание Triangle со стороной C = -Infinity")]
         public void ConstructorWithInvalidSideCThrowsArgumentException(
             double sideA, double sideB, double invalidSideC)
         {
@@ -131,72 +151,91 @@ namespace ModelTests
                 () => new Triangle(sideA, sideB, invalidSideC));
         }
 
-
-        [TestCase(1.0,  2.0,  10.0, TestName = "Triangle: A+B <= C (1+2<=10)")]
-        [TestCase(1.0,  10.0, 2.0,  TestName = "Triangle: A+C <= B (1+2<=10)")]
-        [TestCase(10.0, 1.0,  2.0,  TestName = "Triangle: B+C <= A (1+2<=10)")]
-        [TestCase(1.0,  1.0,  2.0,  TestName = "Triangle: вырожденный (1+1=2)")]
-        public void ConstructorViolatingTriangleInequalityThrowsArgumentException(
+        [TestCase(1.0, 2.0, 10.0,
+            TestName = "Triangle: A+B <= C (1+2<=10)")]
+        [TestCase(1.0, 10.0, 2.0,
+            TestName = "Triangle: A+C <= B (1+2<=10)")]
+        [TestCase(10.0, 1.0, 2.0,
+            TestName = "Triangle: B+C <= A (1+2<=10)")]
+        [TestCase(1.0, 1.0, 2.0,
+            TestName = "Triangle: вырожденный (1+1=2)")]
+        public void
+            ConstructorViolatingTriangleInequalityThrowsArgumentException(
             double sideA, double sideB, double sideC)
         {
             Assert.Throws<ArgumentException>(
                 () => new Triangle(sideA, sideB, sideC));
         }
 
-
-        [TestCase(-1.0,                    TestName = "Validate со стороной A = -1.0")]
-        [TestCase(0.0,                     TestName = "Validate со стороной A = 0.0")]
-        [TestCase(double.NaN,              TestName = "Validate со стороной A = NaN")]
-        [TestCase(double.PositiveInfinity, TestName = "Validate со стороной A = +Infinity")]
-        [TestCase(double.NegativeInfinity, TestName = "Validate со стороной A = -Infinity")]
-        public void ValidateWithInvalidSideAThrowsArgumentException(double invalidSideA)
+        [TestCase(-1.0,
+            TestName = "Validate со стороной A = -1.0")]
+        [TestCase(0.0,
+            TestName = "Validate со стороной A = 0.0")]
+        [TestCase(double.NaN,
+            TestName = "Validate со стороной A = NaN")]
+        [TestCase(double.PositiveInfinity,
+            TestName = "Validate со стороной A = +Infinity")]
+        [TestCase(double.NegativeInfinity,
+            TestName = "Validate со стороной A = -Infinity")]
+        public void ValidateWithInvalidSideAThrowsArgumentException(
+            double invalidSideA)
         {
             var triangle = new Triangle();
             triangle.SideA = invalidSideA;
             triangle.SideB = 4.0;
             triangle.SideC = 5.0;
-
             Assert.Throws<ArgumentException>(() => triangle.Validate());
         }
 
-        [TestCase(-1.0,                    TestName = "Validate со стороной B = -1.0")]
-        [TestCase(0.0,                     TestName = "Validate со стороной B = 0.0")]
-        [TestCase(double.NaN,              TestName = "Validate со стороной B = NaN")]
-        [TestCase(double.PositiveInfinity, TestName = "Validate со стороной B = +Infinity")]
-        [TestCase(double.NegativeInfinity, TestName = "Validate со стороной B = -Infinity")]
-        public void ValidateWithInvalidSideBThrowsArgumentException(double invalidSideB)
+        [TestCase(-1.0,
+            TestName = "Validate со стороной B = -1.0")]
+        [TestCase(0.0,
+            TestName = "Validate со стороной B = 0.0")]
+        [TestCase(double.NaN,
+            TestName = "Validate со стороной B = NaN")]
+        [TestCase(double.PositiveInfinity,
+            TestName = "Validate со стороной B = +Infinity")]
+        [TestCase(double.NegativeInfinity,
+            TestName = "Validate со стороной B = -Infinity")]
+        public void ValidateWithInvalidSideBThrowsArgumentException(
+            double invalidSideB)
         {
             var triangle = new Triangle();
             triangle.SideA = 3.0;
             triangle.SideB = invalidSideB;
             triangle.SideC = 5.0;
-
             Assert.Throws<ArgumentException>(() => triangle.Validate());
         }
 
-        [TestCase(-1.0,                    TestName = "Validate со стороной C = -1.0")]
-        [TestCase(0.0,                     TestName = "Validate со стороной C = 0.0")]
-        [TestCase(double.NaN,              TestName = "Validate со стороной C = NaN")]
-        [TestCase(double.PositiveInfinity, TestName = "Validate со стороной C = +Infinity")]
-        [TestCase(double.NegativeInfinity, TestName = "Validate со стороной C = -Infinity")]
-        public void ValidateWithInvalidSideCThrowsArgumentException(double invalidSideC)
+        [TestCase(-1.0,
+            TestName = "Validate со стороной C = -1.0")]
+        [TestCase(0.0,
+            TestName = "Validate со стороной C = 0.0")]
+        [TestCase(double.NaN,
+            TestName = "Validate со стороной C = NaN")]
+        [TestCase(double.PositiveInfinity,
+            TestName = "Validate со стороной C = +Infinity")]
+        [TestCase(double.NegativeInfinity,
+            TestName = "Validate со стороной C = -Infinity")]
+        public void ValidateWithInvalidSideCThrowsArgumentException(
+            double invalidSideC)
         {
             var triangle = new Triangle();
             triangle.SideA = 3.0;
             triangle.SideB = 4.0;
             triangle.SideC = invalidSideC;
-
             Assert.Throws<ArgumentException>(() => triangle.Validate());
         }
 
-        [Test(Description = "Validate: нарушение неравенства треугольника через setters")]
-        public void ValidateViolatingTriangleInequalityThrowsArgumentException()
+        [Test(Description =
+            "Validate: нарушение неравенства треугольника через setters")]
+        public void
+            ValidateViolatingTriangleInequalityThrowsArgumentException()
         {
             var triangle = new Triangle();
             triangle.SideA = 1.0;
             triangle.SideB = 2.0;
             triangle.SideC = 10.0;
-
             Assert.Throws<ArgumentException>(() => triangle.Validate());
         }
     }
